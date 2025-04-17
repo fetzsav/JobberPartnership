d1fbc20cbad28da55b6c7dc879631f48db79585e
Fetzer <fetz@fetz.dev>
2025-04-17 17:53:23 -0400

    switching to next.js

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+
+import createMDX from '@next/mdx';
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/, // Match both `.md` and `.mdx` files
+  options: {
+    // Add any MDX-specific options here
+  },
+});
+
+const nextConfig = withMDX({
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+  webpack(config, options) {
+    return config;
+  },
+});
+
+export default nextConfig;
